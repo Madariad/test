21769931e544cccb7a5cff1da4fadaa9e2b8679b
Dias <dias.baigazi05@gmail.com>
2024-04-18 04:55:20 +0600

    eev

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), Inspect()],
-  base: "/", 
+  base: "test", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
