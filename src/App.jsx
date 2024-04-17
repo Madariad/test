da15fce21b76b8b5522812db8c5a50a6fffd5964
Dias <dias.baigazi05@gmail.com>
2024-04-18 03:07:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import test from './assets/img/test.jpg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src={test} alt="" />
+    </>
+  )
+}
+
+export default App
